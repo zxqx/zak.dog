d0f930b13858a5b3319268fe1a50e762ae6a4736
Zak Angelle <zaknotzak@gmail.com>
2020-08-23 22:49:57 -0500

    Convert tabs to spaces

--- src/routes/aotw.svelte
+++ src/routes/aotw.svelte
@@ -1,49 +1,86 @@
 <svelte:head>
-	<title>Albums of the Week</title>
+  <title>Albums of the Week</title>
 </svelte:head>
 
 <h5>August 23, 2020</h5>
 
 <div class="album">
-	<img src="./images/album-art/cable-hogue-soundtrack.jpg" alt="Les Rallizes DéNudés - Cable Hogue Soundtrack" class="album-art" />
+  <img
+    src="./images/album-art/cable-hogue-soundtrack.jpg"
+    alt="Les Rallizes DéNudés - Cable Hogue Soundtrack"
+    class="album-art" />
 
-	<div class="album-info">
-		<div class="album-details">
-			<h3>Les Rallizes DéNudés</h3>
-			<h2>Cable Hogue Soundtrack
-				<span class="release-year">(rec. 1971-1979, bootlegged 2007)</span>
-			</h2>
-		</div>
+  <div class="album-info">
+    <div class="album-details">
+      <h3>Les Rallizes DéNudés</h3>
+      <h2>
+        Cable Hogue Soundtrack
+        <span class="release-year">(rec. 1971-1979, bootlegged 2007)</span>
+      </h2>
+    </div>
 
-		<p>Formed in Japan in the late 60s, Les Rallizes DéNudés hadn't actually heard any rock music before, but were basing their sound on what they thought it would sound like after reading descriptions of the work of American artists like Hendrix and Zeppelin. It may take a few listens to break through the thick walls of meandering guitar noise, hypnotic bass lines, and psychedelic lofi production &mdash; which often teeters on the edge of unhinged &mdash; but it's ultimately more than worth it.</p>
-	</div>
+    <p>
+      Formed in Japan in the late 60s, Les Rallizes DéNudés hadn't actually
+      heard any rock music before, but were basing their sound on what they
+      thought it would sound like after reading descriptions of the work of
+      American artists like Hendrix and Zeppelin. It may take a few listens to
+      break through the thick walls of meandering guitar noise, hypnotic bass
+      lines, and psychedelic lofi production &mdash; which often teeters on the
+      edge of unhinged &mdash; but it's ultimately more than worth it.
+    </p>
+  </div>
 </div>
 
 <div class="album">
-	<img src="./images/album-art/come-away-with-esg.jpg" alt="ESG - Come Away With ESG" class="album-art" />
+  <img
+    src="./images/album-art/come-away-with-esg.jpg"
+    alt="ESG - Come Away With ESG"
+    class="album-art" />
 
-	<div class="album-info">
-		<div class="album-details">
-			<h3>ESG</h3>
-			<h2>Come Away With ESG
-				<span class="release-year">(1983)</span>
-			</h2>
-		</div>
+  <div class="album-info">
+    <div class="album-details">
+      <h3>ESG</h3>
+      <h2>
+        Come Away With ESG
+        <span class="release-year">(1983)</span>
+      </h2>
+    </div>
 
-		<p>ESG enters the scene with a bang on their debut release, showcasing their unique blend of infectious vocal lines and a groovy-as-hell rhythmic section. The Scroggins sisters take a minimalist approach to dance punk, and each instrument feels supremely significant. In fact, labeling it "dance punk" is doing a disservice to the songs on this album. <i>Come Away With ESG</i> absolutely demands dancing, and at the same time makes all other dance music sound corny in comparison.</p>
-	</div>
+    <p>
+      ESG enters the scene with a bang on their debut release, showcasing their
+      unique blend of infectious vocal lines and a groovy-as-hell rhythmic
+      section. The Scroggins sisters take a minimalist approach to dance punk,
+      and each instrument feels supremely significant. In fact, labeling it
+      "dance punk" is doing a disservice to the songs on this album.
+      <i>Come Away With ESG</i>
+      absolutely demands dancing, and at the same time makes all other dance
+      music sound corny in comparison.
+    </p>
+  </div>
 </div>
 
 <div class="album">
-	<img src="./images/album-art/you-the-night-and-the-music.jpg" alt="Gallon Drunk - You, The Night... and The Music" class="album-art" />
-	<div class="album-info">
-		<div class="album-details">
-			<h3>Gallon Drunk</h3>
-			<h2>You, The Night... and The Music
-				<span class="release-year">(1992)</span>
-			</h2>
-		</div>
+  <img
+    src="./images/album-art/you-the-night-and-the-music.jpg"
+    alt="Gallon Drunk - You, The Night... and The Music"
+    class="album-art" />
+  <div class="album-info">
+    <div class="album-details">
+      <h3>Gallon Drunk</h3>
+      <h2>
+        You, The Night... and The Music
+        <span class="release-year">(1992)</span>
+      </h2>
+    </div>
 
-		<p>The perfect backdrop for an unruly Tuesday night at the dingy dive bar: the air is punctuated by an unmistakable hint of stale beer and mildew, some crust punk is bumbling around polishing off people's empties, and the door has been ripped off the only bathroom stall you can shit in. Gallon Drunk wears their Birthday Party influence proudly on their sleeve, but manages to make their brand of noisy swamp blues sound convincingly surreal.</p>
-	</div>
+    <p>
+      The perfect backdrop for an unruly Tuesday night at the dingy dive bar:
+      the air is punctuated by an unmistakable hint of stale beer and mildew,
+      some crust punk is bumbling around polishing off people's empties, and the
+      door has been ripped off the only bathroom stall you can shit in. Gallon
+      Drunk wears their Birthday Party influence proudly on their sleeve, but
+      manages to make their brand of noisy swamp blues sound convincingly
+      surreal.
+    </p>
+  </div>
 </div>
