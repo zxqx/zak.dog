a838f578767ef2ce4cc111d9354b1ffb5642b50f
Zak Angelle <zaknotzak@gmail.com>
2020-11-30 19:42:31 -0600

    Update nav

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,3 @@
-<script>
-  import Nav from '../components/Nav.svelte';
-
-  export let segment;
-</script>
-
-<style>
-  main {
-    position: relative;
-    max-width: 100%;
-    background-color: white;
-    padding: 1em 2em 0;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-</style>
-
-<Nav {segment} />
-
 <main>
   <slot />
 </main>
