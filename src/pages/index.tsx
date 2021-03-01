a6a119ff37536ffe0dda35a25369150389253b2c
Zak Angelle <zaknotzak@gmail.com>
2021-03-01 02:29:05 -0600

    update page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const IndexPage = () => {
 
   return (
     <Layout>
-      <SEO title="Home" />
+      <SEO title="Blog" />
       <Animated>
         <PostTeasers>
           {edges.map(edge => (
